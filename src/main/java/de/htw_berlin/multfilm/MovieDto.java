8b3550f2fa6b125fe039a7f519af36ee4711da16
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2026-04-09 01:31:15 +0200

    feat: add javadoc

--- src/main/java/de/htw_berlin/multfilm/MovieDto.java
+++ src/main/java/de/htw_berlin/multfilm/MovieDto.java
@@ -2,6 +2,12 @@ package de.htw_berlin.multfilm;
 
 import java.util.List;
 
+//record für die Film-Details, die von der TMDB API zurückgegeben werden
+//enthält Titel, Übersicht, Poster-URL, Veröffentlichungsdatum, Genres, Cast 
+//und Regisseur
+//wird von MovieService erstellt und von TmdbController zurückgegeben
+//record ist eine spezielle Klasse in Java, die hauptsächlich Daten enthält 
+//und automatisch Getter, equals, hashCode und toString generiert
 public record MovieDto(
         String title,
         String overview,
