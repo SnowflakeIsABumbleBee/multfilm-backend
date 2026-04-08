53a1e071c000880a85531536f21fd2efa206620e
SnowflakeIsABumbleBee <97166092+SnowflakeIsABumbleBee@users.noreply.github.com>
2026-04-08 10:51:18 +0200

    add spring to file

---
+++ src/main/java/de/htw_berlin/multfilm/MultfilmApplication.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.multfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultfilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw_berlin/multfilm/MultfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.multfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
